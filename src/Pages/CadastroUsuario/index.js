ce3538d8a0e718a9cfbe06960347c3dfb399ac8d
Guilherme Augusto <130418882+guidev63@users.noreply.github.com>
2023-12-01 21:38:46 -0300

    alteraçao do cadastro de usuario

--- src/Pages/CadastroUsuario/index.js
+++ src/Pages/CadastroUsuario/index.js
@@ -1,36 +1,51 @@
+import react,{useState} from 'react';
+
+
 import '../../Pages/global.css';
 import Menu from '../../componentes/menu'
 import { FiEdit, FiTrash, FiDelete, FiFilePlus } from "react-icons/fi";
-import { confirmAlert } from 'react-confirm-alert'; // Import
-import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
+import { FaSave } from "react-icons/fa";
+import { MdOutlineCancel } from "react-icons/md";
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 import { Link } from 'react-router-dom';
 import Head from '../../componentes/head';
 
 export default function Cadastrousuario() {
 
+    const[nome,setNome] = useState("");
     return (
-
         <div className="dashboard-container">
-
-
             <div className='menu'>
-
+                <h1>  menu</h1>
                 <Menu />
+
             </div>
             <div className='principal'>
                 <Head title="Cadastro de Usuários" />
+                <div className='form-container'>
+                    <form className="form-cadastro">
+                        <input type='text' value={nome} onChange={e=>setNome(e.target.value)}
 
-                <from>
-                    <input type='Text' placeholder='Digite o nome do usuario' />
-                    <input type='Text' placeholder='Digite seu @email' />
-                    <input type='Text' placeholder='Digite seu senha' />
-                    <button>Salvar</button>
-                    <button>Cancelar</button>
-                </from>
+                        placeholder='Digite o nome do usuário'
+                         />
+                        <input type='text' placeholder='Digite seu email' />
+                        <input type='password' placeholder='Digite sua senha' />
+                        <div>
+                            <button className='btn-save'>
+                                <FaSave />
+                                Salvar
+                            </button>
 
+                            <button className='btn-cancel'>
+                                <MdOutlineCancel />
+                                Cancelar
+                            </button>
+                        </div>
+                    </form>
+                    {nome}
+                </div>
             </div>
-        </div >
-
+        </div>
     )
-
 }
