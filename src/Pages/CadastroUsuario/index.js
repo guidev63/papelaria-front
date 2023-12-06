d491989649ddd22f1a957adbfc3fb39a47e50854
Guilherme Augusto <130418882+guidev63@users.noreply.github.com>
2023-12-05 21:35:26 -0300

    colocamos o local strong

--- src/Pages/CadastroUsuario/index.js
+++ src/Pages/CadastroUsuario/index.js
@@ -1,4 +1,4 @@
-import react,{useState} from 'react';
+import react, { useState } from 'react';
 
 
 import '../../Pages/global.css';
@@ -13,7 +13,23 @@ import Head from '../../componentes/head';
 
 export default function Cadastrousuario() {
 
-    const[nome,setNome] = useState("");
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const usuarios = ["carlos", "carlos@gmail.com", "123"]
+    const usuario = {
+        nome,
+        email,
+        senha
+    }
+    function salvardados(e) {
+        e.preventDefault();
+        // console.log(usuario);
+        const banco = JSON.parse(localStorage.getItem("cd-usuarios") || "[]");
+        banco.push(usuario);
+        localStorage.setItem("cd-usuarios", JSON.stringify(banco));
+        alert("usuario salvo com sucesso");
+    }
     return (
         <div className="dashboard-container">
             <div className='menu'>
@@ -22,15 +38,17 @@ export default function Cadastrousuario() {
 
             </div>
             <div className='principal'>
-                <Head title="Cadastro de Usuários" />
+                <Head title="Cadastro de Usuários"/>
                 <div className='form-container'>
-                    <form className="form-cadastro">
-                        <input type='text' value={nome} onChange={e=>setNome(e.target.value)}
-
-                        placeholder='Digite o nome do usuário'
-                         />
-                        <input type='text' placeholder='Digite seu email' />
-                        <input type='password' placeholder='Digite sua senha' />
+                    <form className='form-cadastro' onSubmit={salvardados} >
+                        <input type='text'
+                            value={nome} onChange={e => setNome(e.target.value)}
+                            placeholder='Digite o nome do usuário'
+                        />
+                        <input type='text'
+                            value={email} onChange={e => setEmail(e.target.value)} placeholder='Digite seu email' />
+                        <input type='password'
+                            value={senha} onChange={e => setSenha(e.target.value)} placeholder='Digite sua senha' />
                         <div>
                             <button className='btn-save'>
                                 <FaSave />
@@ -43,7 +61,7 @@ export default function Cadastrousuario() {
                             </button>
                         </div>
                     </form>
-                    {nome}
+
                 </div>
             </div>
         </div>
